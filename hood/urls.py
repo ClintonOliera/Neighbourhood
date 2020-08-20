1bfe1f2c4126f13d03fc48432aabd61fd5d7d770
ian <shemian092@gmail.com>
2020-08-20 15:57:25 +0300

    add temp cont

--- hood/urls.py
+++ hood/urls.py
@@ -1,18 +1,18 @@
-from django.conf.urls import url, include
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    url('', views.index, name='index'),
-    url('signup/', views.signup, name='signup'),
-    url('account/', include('django.contrib.auth.urls')),
-    url('all-hoods/', views.hoods, name='hood'),
-    url('new-hood/', views.create_hood, name='new-hood'),
-    url('profile/<username>', views.profile, name='profile'),
-    url('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
-    url('join_hood/<id>', views.join_hood, name='join-hood'),
-    url('leave_hood/<id>', views.leave_hood, name='leave-hood'),
-    url('single_hood/<hood_id>', views.single_hood, name='single-hood'),
-    url('<hood_id>/new-post', views.create_post, name='post'),
-    url('<hood_id>/members', views.hood_members, name='members'),
-    url('search/', views.search_business, name='search'),
+    path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
+    path('all-hoods/', views.hoods, name='hood'),
+    path('new-hood/', views.create_hood, name='new-hood'),
+    path('profile/<username>', views.profile, name='profile'),
+    path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
+    path('join_hood/<id>', views.join_hood, name='join-hood'),
+    path('leave_hood/<id>', views.leave_hood, name='leave-hood'),
+    path('single_hood/<hood_id>', views.single_hood, name='single-hood'),
+    path('<hood_id>/new-post', views.create_post, name='post'),
+    path('<hood_id>/members', views.hood_members, name='members'),
+    path('search/', views.search_business, name='search'),
 ]
